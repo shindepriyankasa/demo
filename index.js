687c46b21969b77e70ddf9757dce4e31d6407644
Bulu Sahu <Q2239@Corp.qualitykiosk.com>
2019-06-14 15:51:38 +0530

    check2

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Travis!\n')
+}).listen(3000,'127.0.0.1');
+console.log('Server running at http://127.0.0.1:3000/');
